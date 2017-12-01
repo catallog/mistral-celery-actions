e52647f00ccafb242aa68d665cb3a1e5f5466bed
ariane <arianedematos@gmail.com>
2017-12-01 14:46:14 -0200

    fix: properties setup to install

--- setup.py
+++ setup.py
@@ -12,7 +12,10 @@ setup(
     package_dir={'': '.'},
     py_modules=['celery_actions'],
     pbr=True,
+    description='Mistral actions to execute Celery tasks',
     version='1.0.0',
     author='Collabo Team',
+    download_url='https://github.com/collabo-br/mistral-celery-actions/archive/1.0.0.tar.gz',
+    keywords=['workflow', 'actions', 'mistral', 'celery'],
     install_requires=required
 )
